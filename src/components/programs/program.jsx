34488781ad6b90fa3aef0174ed7ee9b8ee6087e5
AhmedSaeed216 <ahmed.saeed6420@gmail.com>
2024-09-20 10:56:35 +0500

    add the programs section

---
+++ src/components/programs/program.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import "./program.css"
+// import "../../data/programsData.js"
+import { programsData } from '../../data/programsData.js'
+import RightArrow from "../../assets/rightArrow.png"
+const Program = () => {
+    return (
+        <div className="Programs" id="Programs">
+            <div className="Program-Header">
+                <span className='stroke-text'>Explore our </span>
+                <span>Programs </span>
+                <span className='stroke-text'>to Shape you</span>
+            </div>
+
+
+            {/* <div className="programCategeories">
+                {
+                    programsData.map((element) => (
+                        <div className="categeories">
+
+                            {element.image}
+                            <span>
+                                {element.heading},
+                            </span>
+                            <span>
+                                {element.details}
+                            </span>
+                        </div>
+                    ))
+                }
+            </div> */}
+
+            <div className="programCatogerios">
+            {programsData.map((program)=>(
+                <div className='category'>
+                    {program.image}
+                    <span>
+                        {program.heading}
+                    </span>
+                    <span>{program.details}</span>
+
+                    <div className="joinNow">
+                        <span>Join Now</span>
+                        <img src={RightArrow} alt="" />
+                    </div>
+                </div>
+            ))}
+        </div>
+        </div>
+    )
+}
+
+export default Program
